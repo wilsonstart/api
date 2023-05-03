d8de1eaf27c3a5065070a990e8c97b848622baf2
eduardodiasc <eduardodiasc@gmail.com>
2023-05-03 17:25:33 -0300

    atualizacoes api

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'wongames'),
+        database: env('DATABASE_NAME', 'startv1'),
         username: env('DATABASE_USERNAME', 'start'),
         password: env('DATABASE_PASSWORD', 'start123'),
         ssl: env.bool('DATABASE_SSL', false),
